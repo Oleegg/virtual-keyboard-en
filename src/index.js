6605a1d5d64efde34068cc3b57300e360271b1a8
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-05-01 23:46:42 +0300

    init: bigin webpack config

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const heading = document.createElement('h1')
+heading.textContent = 'My virtual keyboard'
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.name}.`
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p)
