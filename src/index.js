fcfe91e5f9d839b3cf395536daef89e822e5f40b
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-05-03 19:00:19 +0300

    feat: setting webpack

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+import './styles/normalize.css'
+import './styles/style.scss'
+import katy from './assets/imges/pets-Katrine.jpg'
 const heading = document.createElement('h1')
 heading.textContent = 'My virtual keyboard'
 // создание свойства класса без конструктора
 class Game {
-    name = 'Violin Charades'
+    name = 'Oleegg'
 }
 const myGame = new Game()
 
 // создаем параграф
 const p = document.createElement('p')
-p.textContent = `I like ${myGame.name}.`
+p.textContent = `My name is ${myGame.name}.`
 
 // добавляем заголовок в DOM
-const root = document.querySelector('#root')
-root.append(heading, p)
+const bbody = document.querySelector('body')
+bbody.append(heading, p)
+
+// create img
+const img = document.createElement('img')
+img.classList.add('img')
+img.src = katy
+img.alt = 'cat'
+bbody.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -8,8 +11,12 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
     },
+    devServer: {
+        port: 8080,
+    },
+    devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'virtual keyboard',
@@ -17,15 +24,45 @@ module.exports = {
             filename: 'index.html', // название выходного файла
         }),
         new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin(
+            {
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, './src/assets/icons/keyboard.ico'),
+                        to: path.resolve(__dirname, './dist')
+                    },
+                ]
+            }
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        })
     ],
     module: {
         rules: [
-            // JavaScript
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: ['babel-loader'],
             },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            },
         ],
-    }
+    },
 }
